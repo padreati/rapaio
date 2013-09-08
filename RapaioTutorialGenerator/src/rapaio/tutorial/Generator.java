8ccee2d62cc13a34b446d1027ea1ed1341ae63c8
tutuianu <fry7swkl12>
2013-09-08 10:45:34 +0300

    some manual try

--- RapaioTutorialGenerator/src/rapaio/tutorial/Generator.java
+++ RapaioTutorialGenerator/src/rapaio/tutorial/Generator.java
@@ -2,12 +2,14 @@ package rapaio.tutorial;
 
 import rapaio.tutorial.pages.HistogramTutorial;
 
+import java.io.IOException;
+
 /**
  * User: <a href="mailto:padreati@yahoo.com">Aurelian Tutuianu</a>
  */
 public class Generator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         HistogramTutorial.main(args);
     }
