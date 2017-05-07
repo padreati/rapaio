26512ae43e16c33cf870cf171da06ab828332fbd
Woojin Lee <holinder4s@gmail.com>
2017-05-07 19:54:46 +0900

    CI build error fix

--- src/rapaio/ml/classifier/bayes/BinaryData.java
+++ src/rapaio/ml/classifier/bayes/BinaryData.java
@@ -1,12 +1,13 @@
 package rapaio.ml.classifier.bayes;
 
+import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
 
 import rapaio.data.Frame;
 import rapaio.ml.classifier.bayes.estimator.BinaryEstimator;
 
-public class BinaryData extends NaiveBayesData{
+public class BinaryData extends NaiveBayesData implements Serializable{
 	public BinaryEstimator binEstimator;
 	public Map<String, BinaryEstimator> binMap;
 
--- src/rapaio/ml/classifier/bayes/NominalData.java
+++ src/rapaio/ml/classifier/bayes/NominalData.java
@@ -1,12 +1,13 @@
 package rapaio.ml.classifier.bayes;
 
+import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
 
 import rapaio.data.Frame;
 import rapaio.ml.classifier.bayes.estimator.NominalEstimator;
 
-public class NominalData extends NaiveBayesData{
+public class NominalData extends NaiveBayesData implements Serializable{
 	public NominalEstimator nomEstimator;
 	public Map<String, NominalEstimator> nomMap;
 
