304dbc95635c8545dd62bcbd29427bacb6a50e32
Aurelian Tutuianu <padreati@yahoo.com>
2019-12-02 11:03:27 +0200

    - fix https://github.com/padreati/rapaio/issues/178

---
+++ src/rapaio/data/filter/FFImputeWithClassifier.java
@@ -0,0 +1,50 @@
+package rapaio.data.filter;
+
+import rapaio.data.Frame;
+import rapaio.data.VRange;
+import rapaio.data.Var;
+import rapaio.ml.classifier.ClassifierModel;
+import rapaio.ml.classifier.ClassifierResult;
+
+/**
+ * Created by <a href="mailto:padreati@yahoo.com">Aurelian Tutuianu</a> on 11/12/19.
+ */
+public class FFImputeWithClassifier extends AbstractFF {
+
+    public static FFImputeWithClassifier of(ClassifierModel model, VRange inputVars, String targetName) {
+        return new FFImputeWithClassifier(model, inputVars, targetName);
+    }
+
+    private static final long serialVersionUID = 7428989420235407246L;
+
+    private final ClassifierModel model;
+    private final String targetName;
+
+    private FFImputeWithClassifier(ClassifierModel model, VRange inputVars, String targetName) {
+        super(inputVars);
+        this.model = model;
+        this.targetName = targetName;
+    }
+
+    @Override
+    protected void coreFit(Frame df) {
+        var selection = df.mapVars(varNames).stream().filter(s -> !s.isMissing(targetName)).toMappedFrame().copy();
+        model.fit(selection, targetName);
+    }
+
+    @Override
+    public Frame apply(Frame df) {
+        var toFill = df.stream().filter(s -> s.isMissing(targetName)).toMappedFrame();
+        ClassifierResult result = model.predict(toFill, true, false);
+        Var prediction = result.firstClasses();
+        for (int i = 0; i < prediction.rowCount(); i++) {
+            toFill.setLabel(i, targetName, prediction.getLabel(i));
+        }
+        return df;
+    }
+
+    @Override
+    public FFImputeWithClassifier newInstance() {
+        return new FFImputeWithClassifier(model.newInstance(), vRange, targetName);
+    }
+}
---
+++ tst/rapaio/data/filter/FFImputeWithClassifierTest.java
@@ -0,0 +1,54 @@
+package rapaio.data.filter;
+
+import org.junit.Before;
+import org.junit.Test;
+import rapaio.core.RandomSource;
+import rapaio.core.stat.Mean;
+import rapaio.data.Frame;
+import rapaio.data.SolidFrame;
+import rapaio.data.VRange;
+import rapaio.data.VarNominal;
+import rapaio.ml.classifier.rule.OneRule;
+
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Created by <a href="mailto:padreati@yahoo.com">Aurelian Tutuianu</a> on 11/28/19.
+ */
+public class FFImputeWithClassifierTest {
+
+    private static final String[] xd = new String[]{"a", "b", "c", "d", "?"};
+    private static final String[] yd = new String[]{"x", "y", "?"};
+
+    @Before
+    public void setUp() {
+        RandomSource.setSeed(123);
+    }
+
+    @Test
+    public void testBasic() {
+
+        VarNominal x = VarNominal.from(100, row -> xd[RandomSource.nextInt(xd.length)]).withName("x");
+        VarNominal y = VarNominal.from(100, row -> yd[RandomSource.nextInt(yd.length)]).withName("y");
+
+        double xm = Mean.of(x).value();
+        double ym = Mean.of(y).value();
+
+        var model = new OneRule();
+
+        FFImputeWithClassifier xfilter = FFImputeWithClassifier.of(model, VRange.of("x,y"), "x").newInstance();
+        FFImputeWithClassifier yfilter = FFImputeWithClassifier.of(model, VRange.of("x,y"), "y");
+
+        Frame df = SolidFrame.byVars(x, y);
+
+        xfilter.fit(df);
+        yfilter.fit(df);
+
+        Frame copy = df.copy().fapply(xfilter, yfilter);
+
+        for (int i = 0; i < 100; i++) {
+            assertFalse(copy.isMissing(i, "x"));
+            assertFalse(copy.isMissing(i, "y"));
+        }
+    }
+}
